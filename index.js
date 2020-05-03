4ac3732919d662c7c9a28b96cbe9e33ab62a7962
Frédéric Bégin <fbegin@nexapp.ca>
2020-05-03 18:34:05 -0400

    cors

--- index.js
+++ index.js
@@ -6,14 +6,7 @@ const db = require('./queries');
 const app = express();
 const port = process.env.PORT || 3000;
 
-var corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
-
-app.options('*', cors());
-app.use(cors(corsOptions));
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
