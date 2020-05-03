73c8822358edd64c79196c27c107814d752af251
Frédéric Bégin <fbegin@nexapp.ca>
2020-05-03 18:28:57 -0400

    cors

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const db = require('./queries');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
   }),
 );
-app.use(cors());
 
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' });
