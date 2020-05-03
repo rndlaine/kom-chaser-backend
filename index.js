84c43511fd4c8d1f8575b707cee18d13f5b22136
Frédéric Bégin <fbegin@nexapp.ca>
2020-05-03 18:39:41 -0400

    try catch

--- index.js
+++ index.js
@@ -18,22 +18,25 @@ app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' });
 });
 
-app.get('/athlete/:id', db.getAthlete);
-app.post('/athlete', db.createAthlete);
-app.get('/athlete/:id/activity', db.getActivitiesByUser);
-app.get('/activity/:id', db.getActivity);
-app.get('/segment/:id', db.getSegment);
-app.get('/gear/:id', db.getEquipment);
-app.get('/activity/:id/segmentefforts', db.getSegmentEffortsByActivity);
-app.get('/athlete/:id/segmentefforts', db.getSegmentEffortsByUser);
-app.get('/athlete/:id/bestsegmentefforts', db.getBestSegmentEffortsByUser);
-app.get('/segment/:id/segmentefforts', db.getSegmentEffortsBySegment);
-app.get('/segmentefforts/:id', db.getSegmentEffort);
-app.get('/segment/:id/leaderboard', db.getSegmentLeaderboard);
-app.post('/athlete/:id/sync-activity', db.syncActivity);
-app.post('/athlete/:id/sync-efforts', db.syncSegmentEfforts);
-app.post('/athlete/:id/sync-leaderboard', db.syncLeaderboard);
-
+try {
+  app.get('/athlete/:id', db.getAthlete);
+  app.post('/athlete', db.createAthlete);
+  app.get('/athlete/:id/activity', db.getActivitiesByUser);
+  app.get('/activity/:id', db.getActivity);
+  app.get('/segment/:id', db.getSegment);
+  app.get('/gear/:id', db.getEquipment);
+  app.get('/activity/:id/segmentefforts', db.getSegmentEffortsByActivity);
+  app.get('/athlete/:id/segmentefforts', db.getSegmentEffortsByUser);
+  app.get('/athlete/:id/bestsegmentefforts', db.getBestSegmentEffortsByUser);
+  app.get('/segment/:id/segmentefforts', db.getSegmentEffortsBySegment);
+  app.get('/segmentefforts/:id', db.getSegmentEffort);
+  app.get('/segment/:id/leaderboard', db.getSegmentLeaderboard);
+  app.post('/athlete/:id/sync-activity', db.syncActivity);
+  app.post('/athlete/:id/sync-efforts', db.syncSegmentEfforts);
+  app.post('/athlete/:id/sync-leaderboard', db.syncLeaderboard);
+} catch (e) {
+  console.log(e.message);
+}
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
 });
